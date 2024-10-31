2f1954a2bf7a5e1d4bf8020ecb11f143136f9946
KhaledHAnter <khaldntr11@gmail.com>
2024-10-31 14:28:53 +0300

    change app icon

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'fashion_flare.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
@@ -13,9 +13,9 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-    statusBarColor: Colors.transparent,
-  ));
+  // SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+  //   statusBarColor: Colors.transparent,
+  // ));
   await setupGetIt(); // Initialize dependencies
   runApp(
     // DevicePreview(
